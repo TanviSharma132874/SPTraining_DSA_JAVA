18f137befb4c2c790b4aca79d3f80ee566cb7c6e
TanviSharma132874 <tanvisharma352007@gmail.com>
2026-09-14 19:21:32 +0530

    Java DSA Special Training Practice....

--- src/class_5/Rev_Num.java
+++ src/class_5/Rev_Num.java
@@ -14,4 +14,21 @@ public class Rev_Num {
         }
         System.out.println(rev_num);
     }
+
 }
+
+//
+//class Solution {
+//    public int reverse(int x) {
+//        long rev_num = 0;
+//        while(x != 0){
+//            int rem = x % 10;
+//
+//
+//            rev_num = rev_num * 10 + rem;
+//            x = x/10;
+//        }
+//        if(rev_num < Integer.MIN_VALUE || rev_num > Integer.MAX_VALUE){
+//            return 0;
+//        }
+//        return (int) rev_num;
